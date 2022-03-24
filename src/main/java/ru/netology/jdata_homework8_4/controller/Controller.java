f0c53e6f45dfcfb50559fd2d3bd0867346059e1f
Alexey Nazarenko <hawksolid@gmail.com>
2022-03-24 20:35:00 +0300

    jpa-repository added

--- src/main/java/ru/netology/jdata_homework8_4/controller/Controller.java
+++ src/main/java/ru/netology/jdata_homework8_4/controller/Controller.java
@@ -1,13 +1,12 @@
 package ru.netology.jdata_homework8_4.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.netology.jdata_homework8_4.model.Person;
 import ru.netology.jdata_homework8_4.model.Persons;
 import ru.netology.jdata_homework8_4.repository.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/persons")
 @RestController
@@ -21,7 +20,43 @@ public class Controller {
 
     @GetMapping("/by-city")
     public List<Persons> getPersonsByCity(@RequestParam("city") String city) {
-        return repository.getPersonsByCity(city);
+        return repository.findPersonsByCityOfLiving(city);
+    }
+
+    @GetMapping("/by-age-less-than")
+    public List<Persons> getPersonsByAge(@RequestParam("age") int age) {
+        return repository.findPersonsByPersonAgeLessThan(age);
+    }
+
+    @GetMapping("/by-name-surname")
+    public Optional<List<Persons>> getPersonsByNameSurname(@RequestParam("name") String name,
+                                           @RequestParam("surname") String surname) {
+        return repository.findPersonsByPersonNameAndPersonSurname(name, surname);
+    }
+
+    @GetMapping("/all")
+    public List<Persons> getPersonsAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/count")
+    public long getCount() {
+        return repository.count();
+    }
+
+    @PostMapping("/create")
+    public Persons savePersons(@RequestBody Persons persons) {
+        return repository.save(persons);
+    }
+
+    @DeleteMapping("/delete-person")
+    public void deletePerson(@RequestBody Person person) {
+        repository.deleteById(person);
+    }
+
+    @DeleteMapping("/delete-all")
+    public void deleteAll() {
+        repository.deleteAll();
     }
 }
 
--- src/main/java/ru/netology/jdata_homework8_4/repository/Repository.java
+++ src/main/java/ru/netology/jdata_homework8_4/repository/Repository.java
@@ -1,24 +1,20 @@
 package ru.netology.jdata_homework8_4.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.netology.jdata_homework8_4.model.Person;
 import ru.netology.jdata_homework8_4.model.Persons;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
 @org.springframework.stereotype.Repository
-public class Repository {
+public interface Repository extends JpaRepository<Persons, Person> {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    List<Persons> findPersonsByCityOfLiving(String city);
 
-    public Repository(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
+    List<Persons> findPersonsByPersonAgeLessThan(int age);
 
-    public List<Persons> getPersonsByCity(String city) {
-        return entityManager.createQuery("select p from Persons p where lower(p.cityOfLiving) = :city", Persons.class)
-                .setParameter("city", city)
-                .getResultList();
-    }
+    Optional<List<Persons>> findPersonsByPersonNameAndPersonSurname(String name, String surname);
+
+
 }
